cc27fcd52e7f8cb58ac19e8f1bf78f0481f73b83
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-01-31 14:38:03 -0300

    :sparkles: utilizando o middleware de autenticação jwt na rota dos recipients

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,16 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
 
 routes.get('/recipients', RecipientController.index);
 routes.get('/recipients/:id', RecipientController.show);
+
+routes.use('/recipients', authMiddleware);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
