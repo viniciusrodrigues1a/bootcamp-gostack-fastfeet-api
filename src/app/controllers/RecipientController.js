59ac35af10da30c8b2eb198972a02ea90aca0ec1
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-01-31 12:27:19 -0300

    :sparkles: validação dos dados de entrada do método de criação

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,7 +1,22 @@
+import * as Yup from 'yup';
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      house_number: Yup.string().required(),
+      complement: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      cep_code: Yup.string().required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.json({ error: 'Validation failed' });
+    }
+
     const recipient = await Recipient.create(req.body);
 
     return res.json(recipient);
