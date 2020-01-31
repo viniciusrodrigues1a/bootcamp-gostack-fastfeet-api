7316dabbcedf0792467de3fe09a04da6b0fb3984
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-01-31 13:08:49 -0300

    expondo a rota '/recipients'

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
 
 const routes = new Router();
 
-routes.post('/session', SessionController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:id', RecipientController.update);
 
 export default routes;
