1f0f9695d8df2ac4bfe05a5866c20097a4d6fa71
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-02-09 19:55:50 -0300

    :sparkles: expondo a rota '/deliverymen'

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -12,7 +13,13 @@ routes.post('/sessions', SessionController.store);
 routes.get('/recipients', RecipientController.index);
 routes.get('/recipients/:id', RecipientController.show);
 
-routes.use('/recipients', authMiddleware);
+routes.use(authMiddleware);
+
+routes.get('/deliverymen', DeliverymanController.index);
+routes.post('/deliverymen', DeliverymanController.store);
+routes.get('/deliverymen/:id', DeliverymanController.show);
+routes.put('/deliverymen/:id', DeliverymanController.update);
+routes.delete('/deliverymen/:id', DeliverymanController.destroy);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
