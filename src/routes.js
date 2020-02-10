704fc2e00d25f678ed915bae80552cb8a4eaedc7
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-02-09 22:42:55 -0300

    :sparkles: adicionando middleware do multer

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
@@ -8,6 +10,7 @@ import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
