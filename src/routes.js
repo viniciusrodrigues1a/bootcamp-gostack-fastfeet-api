4dce6aa764b9d561e57b29b74dc6e9c8287ebbe7
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-02-12 20:38:01 -0300

    :sparkles: expondo a rota '/deliveries'

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import DeliveryController from './app/controllers/DeliveryController';
 import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
@@ -24,6 +25,12 @@ routes.post('/deliverymen', DeliverymanController.store);
 routes.get('/deliverymen/:id', DeliverymanController.show);
 routes.put('/deliverymen/:id', DeliverymanController.update);
 routes.delete('/deliverymen/:id', DeliverymanController.destroy);
+
+routes.get('/deliveries/', DeliveryController.index);
+routes.post('/deliveries/', DeliveryController.store);
+routes.get('/delivery/:id', DeliveryController.show);
+routes.put('/deliveries/:id', DeliveryController.update);
+routes.delete('/deliveries/:id', DeliveryController.destroy);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
