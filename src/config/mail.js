aaece07d0a87743d513cfaa8b3fb1854fe70e902
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-02-13 20:50:38 -0300

    :wrench: configurando nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,11 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'ebe8f023be0b37',
+    pass: '8b573aa1441f1d'
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>'
+  }
+};
