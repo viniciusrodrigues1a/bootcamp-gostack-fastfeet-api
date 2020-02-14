c662623cb9a0036435e5a62c96f385436cb262fa
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-02-13 22:51:52 -0300

    :sparkles: controller de deliveries disponíveis para determinado deliveryman

---
+++ src/app/controllers/AvailableDeliveries.js
@@ -0,0 +1,59 @@
+import Deliveryman from '../models/Deliveryman';
+import Delivery from '../models/Delivery';
+import Recipient from '../models/Recipient';
+import File from '../models/File';
+
+class AvailableDeliveryController {
+  async index(req, res) {
+    const { id } = req.params;
+
+    const deliveries = await Delivery.findAll({
+      where: { deliveryman_id: id },
+      attributes: [
+        'id',
+        'recipient_id',
+        'deliveryman_id',
+        'signature_id',
+        'product',
+        'start_date',
+        'end_date'
+      ],
+      include: [
+        {
+          model: Recipient,
+          as: 'recipient',
+          attributes: [
+            'id',
+            'name',
+            'street',
+            'house_number',
+            'complement',
+            'state',
+            'city',
+            'cep_code'
+          ]
+        },
+        {
+          model: Deliveryman,
+          as: 'deliveryman',
+          attributes: ['id', 'name', 'email']
+        },
+        {
+          model: File,
+          as: 'signature',
+          attributes: ['id', 'name', 'path', 'url_path']
+        }
+      ]
+    });
+
+    if (deliveries.length === 0) {
+      return res
+        .status(400)
+        .json({ error: 'There are no deliveries for this deliveryman' });
+    }
+
+    return res.json(deliveries);
+  }
+}
+
+export default new AvailableDeliveryController();
